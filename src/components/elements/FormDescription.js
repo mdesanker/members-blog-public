ca1a43fef89fc1c5dbc708d9de412f13aa160ef0
Michael Desanker <mdesanker@gmail.com>
2022-01-10 16:50:16 -0500

    Add store provider to app

--- src/components/elements/FormDescription.js
+++ src/components/elements/FormDescription.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 const FormDescription = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
   font-size: 1.1rem;
-  text-align: center;
   padding-bottom: 0.5rem;
 `;
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from "./App";
 import { ThemeProvider } from "styled-components";
 import Theme from "./theme/Theme";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={Theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={Theme}>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
