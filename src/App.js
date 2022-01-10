c8f8c5faa0aaf4686776abe23a538c4de85af101
Michael Desanker <mdesanker@gmail.com>
2022-01-10 07:04:02 -0500

    Set up routing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Landing from "./views/Landing";
+import Dashboard from "./views/Dashboard";
 
 const App = () => {
   return (
     <Fragment>
       <GlobalStyles />
       <Header />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Dashboard />} />
+      </Routes>
       <Footer />
     </Fragment>
   );
---
+++ src/views/Dashboard.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Dashboard = () => {
+  return <DashboardWrapper></DashboardWrapper>;
+};
+
+const DashboardWrapper = styled.main``;
+
+export default Dashboard;
