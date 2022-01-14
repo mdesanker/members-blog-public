6927e2e4a62f7d6aec4eb9338466ada769d62402
Michael Desanker <mdesanker@gmail.com>
2022-01-14 17:09:33 -0500

    Load user on dashboard to check for user change

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Post from "./components/post/Post";
 
 const App = () => {
   useEffect(() => {
+    console.log("LOADING USER");
     store.dispatch(loadUser());
   }, []);
 
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,23 +5,15 @@ import styled from "styled-components";
 import { fetchAllPosts, fetchPostById } from "../../store/slices/postsSlice";
 import BlogPostCard from "./BlogPostCard";
 import Title from "../elements/Title";
+import { loadUser } from "../../store/slices/userSlice";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
   console.log(posts);
 
-  const post = {
-    id: "sdgho39",
-    author: "Michael",
-    title: "Post Title",
-    content:
-      "It's important that we only try to fetch the list of posts once. If we do it every time the <PostsList> component renders, or is re-created because we've switched between views, we might end up fetching the posts several times. We can use the posts.status enum to help decide if we need to actually start fetching, by selecting that into the component and only starting the fetch if the status is 'idle'.",
-    likes: 27,
-    date: "Jan 13, 2022",
-  };
-
   useEffect(() => {
+    dispatch(loadUser());
     dispatch(fetchAllPosts());
   }, []);
 
